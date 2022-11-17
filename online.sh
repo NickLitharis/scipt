f117bd30dbb2785051884ed1784f9d8047dcbf16
NickLitharis <p19lith@ionio.gr>
2022-11-17 19:24:39 +0200

    updated

--- online.sh
+++ online.sh
@@ -1,9 +1,11 @@
-# !bin/sh
+#!/bin/sh
 
-task list > ~/tasklist/list.md
+cd /home/marocos/Desktop/script/
+
+task next > ~/tasklist/list.md
 
 pandoc -o _site/index.html ~/tasklist/list.md
 
-netlify build && netlify deploy --prod
+netlify build --silent && netlify deploy --prod --silent
 
 
